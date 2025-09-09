3c60748e83ab4848113404051ef98e3c568020e7
Ashutosh-03547 <pathyashutosh65@gmail.com>
2025-09-09 18:39:44 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,61 +2,34 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  // check if user is logged in (based on token in localStorage)
-  const isLoggedIn = !!localStorage.getItem("token");
-
   return (
     <div>
-      {/* Navbar */}
       <nav
         style={{
-          background: "#0b141a",
-          padding: "15px 40px",
+          background: "#1f1f1f",
+          padding: "10px 20px",
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
-          borderBottom: "1px solid #222",
-          position: "sticky",
-          top: 0,
-          zIndex: 1000,
         }}
       >
-        {/* Logo */}
         <Link
           to="/"
           style={{
             color: "#25D366",
-            fontSize: "22px",
+            fontSize: "1.5rem",
             fontWeight: "bold",
             textDecoration: "none",
           }}
         >
           SocketChat
         </Link>
-
-        {/* Show Login only if user not logged in */}
-        {!isLoggedIn && (
-          <Link
-            to="/login"
-            style={{
-              padding: "8px 16px",
-              backgroundColor: "#25D366",
-              color: "#121212",
-              borderRadius: "6px",
-              fontWeight: "bold",
-              textDecoration: "none",
-            }}
-          >
-            Login
-          </Link>
-        )}
       </nav>
 
-      {/* Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
